59b8a6d080595a4ad522536fd04a9f35fe6f6f7e
tedcorbett-byte <tedcorbett@gmail.com>
2026-04-02 18:34:02 -0700

    Add 8 new bars, expand subreddits and trigger phrases

--- shared/bars.py
+++ shared/bars.py
@@ -4,26 +4,65 @@ All agents import from here.
 """
 
 BARS = [
-    {"name": "Frontier Room",                "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/frontier-room-seattle"},
-    {"name": "Jade Pagoda",                  "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/jade-pagoda-seattle"},
-    {"name": "Manray",                       "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/manray-seattle"},
-    {"name": "Purr Cocktail Lounge",         "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/purr-seattle"},
-    {"name": "R Place",                      "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/r-place-seattle"},
-    {"name": "Re-Bar",                       "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/re-bar-seattle"},
-    {"name": "Red Door",                     "city": "Seattle",  "state": "WA", "url": "https://pickledeggsco.com/products/the-red-door-seattle"},
-    {"name": "Glacier Lanes",                "city": "Everett",  "state": "WA", "url": "https://pickledeggsco.com/products/glacier-lanes-everett-wa"},
-    {"name": "Bonnie Brae Tavern",           "city": "Denver",   "state": "CO", "url": "https://pickledeggsco.com/products/bonnie-brae-denver"},
-    {"name": "Rock Island",                  "city": "Denver",   "state": "CO", "url": "https://pickledeggsco.com/products/rock-island-denver"},
-    {"name": "Ogden Street South",           "city": "Denver",   "state": "CO", "url": "https://pickledeggsco.com/products/ogden-st-south-denver"},
-    {"name": "Rainbow Music Hall",           "city": "Denver",   "state": "CO", "url": "https://pickledeggsco.com/products/rainbow-music-hall-seattle"},
-    {"name": "Fabulous Matterhorn Supper Club", "city": "Boulder", "state": "CO", "url": "https://pickledeggsco.com/products/unisex-softstyle-t-shirt"},
+    # Seattle, WA
+    {"name": "Frontier Room",                    "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/frontier-room-seattle"},
+    {"name": "Jade Pagoda",                      "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/jade-pagoda-seattle"},
+    {"name": "Manray",                           "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/manray-seattle"},
+    {"name": "Purr Cocktail Lounge",             "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/purr-seattle"},
+    {"name": "R Place",                          "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/r-place-seattle"},
+    {"name": "Re-Bar",                           "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/re-bar-seattle"},
+    {"name": "Red Door",                         "city": "Seattle",    "state": "WA", "url": "https://pickledeggsco.com/products/the-red-door-seattle"},
+    # Everett, WA
+    {"name": "Glacier Lanes",                    "city": "Everett",    "state": "WA", "url": "https://pickledeggsco.com/products/glacier-lanes-everett-wa"},
+    # Denver, CO
+    {"name": "Bonnie Brae Tavern",               "city": "Denver",     "state": "CO", "url": "https://pickledeggsco.com/products/bonnie-brae-denver"},
+    {"name": "Rock Island",                      "city": "Denver",     "state": "CO", "url": "https://pickledeggsco.com/products/rock-island-denver"},
+    {"name": "Ogden Street South",               "city": "Denver",     "state": "CO", "url": "https://pickledeggsco.com/products/ogden-st-south-denver"},
+    {"name": "Rainbow Music Hall",               "city": "Denver",     "state": "CO", "url": "https://pickledeggsco.com/products/rainbow-music-hall-seattle"},
+    # Boulder, CO
+    {"name": "Fabulous Matterhorn Supper Club",  "city": "Boulder",    "state": "CO", "url": "https://pickledeggsco.com/products/unisex-softstyle-t-shirt"},
+    # Bloomington, IN
+    {"name": "Peanut Barrel",                    "city": "Bloomington","state": "IN", "url": "https://pickledeggsco.com/products/peanut-barrel-bloomington"},
+    # Ithaca, NY
+    {"name": "Royal Palm Tavern",                "city": "Ithaca",     "state": "NY", "url": "https://pickledeggsco.com/products/royal-palm-tavern-ithaca"},
+    # Cambridge, MA
+    {"name": "The Tasty",                        "city": "Cambridge",  "state": "MA", "url": "https://pickledeggsco.com/products/the-tasty-cambridge-1"},
+    # Lawrence, KS
+    {"name": "Cadillac Ranch",                   "city": "Lawrence",   "state": "KS", "url": "https://pickledeggsco.com/products/cadillac-ranch-lawrence"},
+    # Rosemead, CA
+    {"name": "Bahooka",                          "city": "Rosemead",   "state": "CA", "url": "https://pickledeggsco.com/products/copy-of-bahooka-rosemead-ca"},
+    # Arlington, VA
+    {"name": "Bardo Rodeo",                      "city": "Arlington",  "state": "VA", "url": "https://pickledeggsco.com/products/bardo-rodeo-arlington"},
+    # Hanover, NH
+    {"name": "Everything But Anchovies",         "city": "Hanover",    "state": "NH", "url": "https://pickledeggsco.com/products/everything-but-anchovies-hanover"},
+    # Cincinnati, OH
+    {"name": "Sunlite Pool",                     "city": "Cincinnati", "state": "OH", "url": "https://pickledeggsco.com/products/sunlite-pool-coney-island-ohio"},
 ]
 
 SUBREDDITS = [
-    "SeattleWA", "Seattle", "Denver", "Colorado",
-    "gaybars", "divebars", "nostalgia", "Bars",
-    "AskSeattle", "AskDenver",
-    "LGBTQ", "gay", "LGBTHistory",
+    # Pacific Northwest
+    "SeattleWA", "Seattle", "AskSeattle", "SeattleHistory", "washingtonstate", "Everett",
+    # Colorado
+    "Denver", "Colorado", "AskDenver", "DenverHistory", "Boulder",
+    # Indiana (Peanut Barrel)
+    "bloomington", "IndianaUniversity",
+    # New York (Royal Palm Tavern)
+    "ithaca", "Cornell",
+    # Massachusetts (The Tasty)
+    "cambridge", "boston",
+    # Kansas (Cadillac Ranch)
+    "lawrence",
+    # California (Bahooka)
+    "LosAngeles",
+    # Ohio (Sunlite Pool)
+    "cincinnati",
+    # Virginia (Bardo Rodeo)
+    "nova", "arlington",
+    # New Hampshire (Everything But Anchovies)
+    "NewHampshire", "Dartmouth",
+    # General bar / nostalgia / LGBTQ
+    "gaybars", "divebars", "nostalgia", "Bars", "BarCulture",
+    "LGBTQ", "LGBT", "gay", "LGBTHistory",
     "whatisthisplace",
 ]
 
@@ -34,10 +73,24 @@ TRIGGER_PHRASES = [
     "looking for a gift", "gift for someone who", "gift idea", "dive bar gift",
     "bar shirt", "bar tshirt", "bar t-shirt", "bar merch",
     "gay bar closed", "lesbian bar closed", "queer bar closed",
-    "Capitol Hill bar", "Pike Pine bar", "Belltown bar",
-    "Colfax bar", "Denver bar", "Boulder bar",
-    "dive bar nostalgia", "old seattle", "old denver",
-    "neighborhood bar gone", "bar is gone", "used to drink at",
+    # Seattle
+    "Capitol Hill bar", "Pike Pine bar", "Belltown bar", "old seattle",
+    # Denver / Boulder
+    "Colfax bar", "Denver bar", "Boulder bar", "old denver",
+    # Bloomington
+    "Bloomington bar", "IU bar",
+    # Ithaca
+    "Ithaca bar", "Cornell bar",
+    # Cambridge
+    "Cambridge bar", "Harvard Square bar",
+    # Lawrence
+    "Lawrence bar", "KU bar",
+    # Cincinnati
+    "Cincinnati bar",
+    # Arlington / DC
+    "Arlington bar",
+    # General
+    "dive bar nostalgia", "neighborhood bar gone", "bar is gone", "used to drink at",
 ]
 
 # Preformatted string used in Claude prompts
